0f4996fb5d8ed0ee67aad7823835a10179f81ef6
PabloAguirre9944 <90850459+PabloAguirre9944@users.noreply.github.com>
2021-09-16 13:14:25 -0500

    Figura de Python

---
+++ main.py
@@ -0,0 +1,24 @@
+import turtle
+Ventana = turtle.Screen()
+tortuga = turtle.Turtle()
+#Triangulo
+'''tortuga.forward(100)
+tortuga.left(120)
+tortuga.forward(100)
+tortuga.left(120)
+tortuga.forward(100)
+tortuga.left(120)
+tortuga.forward(50)
+tortuga.left(90)
+tortuga.forward(87)
+tortuga.right(270)
+tortuga.circle(59)
+turtle.mainloop()'''
+cont=0
+while cont<=10:
+    print("hello")
+    cont=cont+1
+
+print("hello")
+
+
